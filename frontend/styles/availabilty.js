4441cd8da65d104890c5b2ec094bcefc779cf1b4
Saad982 <sheikhsaad671@gmail.com>
2025-06-25 18:04:30 +0100

    added a eventlistener For Dom content loaded and checking weather my elements are gitting by ID

---
+++ frontend/styles/availabilty.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const targetDateInput = document.getElementById('targetDate');
+  const freeList = document.getElementById('freeList');
+  const partialList = document.getElementById('partialList');
+  const fullList = document.getElementById('fullList');
+
+ 
+});
